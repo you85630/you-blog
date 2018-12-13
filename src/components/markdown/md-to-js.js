65f16fc3ee4bf03bdef1618d1f16a2b70244f864
You <you85630@foxmail.com>
2018-12-13 13:19:26 +0800

    更换文件存储位置

--- src/components/markdown/md-to-js.js
+++ src/components/markdown/md-to-js.js
@@ -1,17 +1,17 @@
-const fs = require("fs")
+const fs = require('fs')
 
 let ItemList = {}
 let jsonList = []
-fs.readdir("./", function (err, files) {
+fs.readdir('./', function (err, files) {
   if (err) {
-    return console.error(err);
+    return console.error(err)
   }
   files.forEach(function (file) {
     if (file.slice(-2) == 'md') {
       // 读取文件
       fs.readFile(file, function (err, data) {
         if (err) {
-          return console.log(err);
+          return console.log(err)
         }
         let list = []
         let content = null
@@ -23,29 +23,28 @@ fs.readdir("./", function (err, files) {
             if (i <= 7) {
               list.push({
                 name: key[0],
-                text: key.slice(1, )
+                text: key.slice(1)
               })
             }
           }
         }
         let text = []
         for (let j = 0; j < list.length; j++) {
-          const e = list[j];
-          if (e.name==='tag') {
+          const e = list[j]
+          if (e.name === 'tag') {
             for (let k = 0; k < e.text.length; k++) {
               const t = e.text[k].slice(2)
               text.push(t)
             }
             e.text = text
-
           } else {
             e.text = e.text[0]
           }
         }
 
         content = data.toString()
-        let index = content.lastIndexOf("# 内容")
-        content = content.substring(index + 6, content.length);
+        let index = content.lastIndexOf('# 内容')
+        content = content.substring(index + 6, content.length)
         list.push({
           name: 'info',
           text: content
@@ -57,19 +56,18 @@ fs.readdir("./", function (err, files) {
           }
         }
 
-
         // 准备生成文件
         jsonList.push(nowList)
-        ItemList.homeItemList=jsonList
-        ItemList.total=jsonList.length
+        ItemList.homeItemList = jsonList
+        ItemList.total = jsonList.length
 
-        fs.writeFile('itemList.json', JSON.stringify(ItemList), function (err) {
+        fs.writeFile('./../../../static/itemList.json', JSON.stringify(ItemList), function (err) {
           if (err) {
-            return console.log(err);
+            return console.log(err)
           }
-          console.log('文件生成成功');
+          console.log('文件生成成功')
         })
       })
     }
-  });
-});
+  })
+})
